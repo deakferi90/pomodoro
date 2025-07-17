3a32d3c62f4ee374b8a9800f2d1215bc0266a907
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2025-07-17 16:56:54 +0300

    changes done for countdown

--- src/app/timer/timer.ts
+++ src/app/timer/timer.ts
@@ -1,6 +1,4 @@
 import { Component, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
-// import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-timer',
@@ -10,4 +8,6 @@ import { Component, signal } from '@angular/core';
 })
 export class Timer {
   protected readonly title = signal('pomodoro-app');
+  public play = 'PLAY';
+  public pause = 'PAUSE';
 }
